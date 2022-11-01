44ec7b7c46475041b62946c774ce26b038d79f4d
karkar <yiam.kar.kar@huawei.com>
2022-11-01 13:58:03 +0800

    update files

--- terraform-hwcloud-modules/terraform-hwcloud-cts-local/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cts_sys_id" {
-  value       = huaweicloud_cts_tracker.default.id
-  description = "VPC ID"
-}
-
-output "cts_data_id" {
-  value       = huaweicloud_cts_data_tracker.default.id
-  description = "VPC ID"
-}
--- terraform-hwcloud-modules/terraform-hwcloud-cts-notification/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform { 
-  required_version = ">= 1.1.9"
-  required_providers {
-    huaweicloud = {
-      source  = "huaweicloud/huaweicloud"
-      version = "~> 1.37.1"
-    }
-  }
-}
