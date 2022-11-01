cba0890a29166db21bf820c7cc0f697ec25834b7
karkar <yiam.kar.kar@huawei.com>
2022-11-01 18:19:48 +0800

    update files

--- global-variables-modules/global_vars.tf
+++ global-variables-modules/global_vars.tf
@@ -6,6 +6,6 @@ output "default_region" { value = "ap-southeast-2" }
 
 ## AK/SK for general account 
 output "general_service_account_name"   { value = "general_service" }
-output "general_service_root_account_name"   { value = "" }
-output "general_service_account_ak"   { value = "" }
-output "general_service_account_sk"   { value = "" }
+output "general_service_root_account_name"   { value = "hwstaff_intl_apac_kar_commom" }
+output "general_service_account_ak"   { value = "YLMA7Z0JAR6LTAHMM0IH" }
+output "general_service_account_sk"   { value = "tfd9aRffd7yntkHCdETv02MgrLs4wHJMRHyxGGrV" }
